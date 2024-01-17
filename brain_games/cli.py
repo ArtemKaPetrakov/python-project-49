7366e56a760afc5577e0d0f81b35251c8ed73c1e
Artem Petrakov <artem.petrakov@climb-lab.ru>
2024-01-17 19:14:24 +0300

    step 3

---
+++ brain_games/cli.py
@@ -0,0 +1,5 @@
+import prompt #импорт библиотеки для общения с пользователем
+
+def welcom_user():
+  name = prompt.string('May I have your name? ')
+  print(f'Hello, {name}!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+from brain_games.cli import welcom_user
+
 def main():
   print('Welcome to the Brain Games!')
-
+  welcom_user()
 
 if __name__ == '__main__':
     main()

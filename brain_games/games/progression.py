3e4da46157bc65f8a10e154d0c1065daf701a353
Artem Petrakov <artem.petrakov@climb-lab.ru>
2024-01-22 20:15:39 +0300

    add preparation for brain-progression game

---
+++ brain_games/games/progression.py
@@ -0,0 +1,20 @@
+import random
+
+
+def run_game():
+    # rule = 'Find the greatest common divisor of given numbers.'
+
+    # random_num1 = int(random.randrange(1, 100))
+    # random_num2 = int(random.randrange(1, 100))
+    # question = f'Question: {random_num1} {random_num2}'
+
+    # min_number = random_num1 if random_num1 < random_num2 else random_num2
+    # count = min_number
+    # while count > 0:
+    #     if random_num1 % count == 0 and random_num2 % count == 0:
+    #         correct_answer = str(count)
+    #         break
+    #     else:
+    #         count -= 1
+
+    return (question, correct_answer, rule)
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+from brain_games.game_engine import game_run
+from brain_games.games import progression
+
+
+def main():
+    # even_game()
+    game_run(progression)
+
+
+if __name__ == '__main__':
+    main()

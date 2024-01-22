4a3124a7615414b1ced5925a93e96fd182c79043
Artem Petrakov <artem.petrakov@climb-lab.ru>
2024-01-22 21:18:57 +0300

    step 8 finish

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,19 +2,21 @@ import random
 
 
 def run_game():
-    # rule = 'Find the greatest common divisor of given numbers.'
+    rule = 'What number is missing in the progression?'
+    progresion_length = 10
+    random_element = int(random.randrange(0, 9))
+    progresion_increment = int(random.randrange(1, 10))
+    star_number = int(random.randrange(1, 50))
+    result = []
+    count = 0
 
-    # random_num1 = int(random.randrange(1, 100))
-    # random_num2 = int(random.randrange(1, 100))
-    # question = f'Question: {random_num1} {random_num2}'
-
-    # min_number = random_num1 if random_num1 < random_num2 else random_num2
-    # count = min_number
-    # while count > 0:
-    #     if random_num1 % count == 0 and random_num2 % count == 0:
-    #         correct_answer = str(count)
-    #         break
-    #     else:
-    #         count -= 1
+    while count < progresion_length:
+        result.append(str(star_number))
+        star_number += progresion_increment
+        count += 1
+    correct_answer = str(result[random_element])
+    result[random_element] = '..'
+    result = ' '.join(result)
+    question = f'Question: {result}'
 
     return (question, correct_answer, rule)

9bc9604af74099c5aa6d183bfe4025dbc88c7875
Artem Petrakov <artem.petrakov@climb-lab.ru>
2024-01-24 01:17:54 +0300

    fix in even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,7 +2,7 @@ import random
 
 
 def run_game():
-    rule = 'Answer "yes" if number even otherwise answer "no".'
+    rule = 'Answer "yes" if the number is even, otherwise answer "no".'
     random_number = random.randrange(0, 100)
     question = f'Question: {random_number}'
     correct_answer = 'yes' if (random_number % 2 == 0) else 'no'

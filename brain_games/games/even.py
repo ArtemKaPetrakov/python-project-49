87d90c882bc843bf52c42f51c86e114d1b2473d6
Artem Petrakov <artem.petrakov@climb-lab.ru>
2024-01-24 01:10:01 +0300

    fix

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -3,7 +3,7 @@ import random
 
 def run_game():
     rule = 'Answer "yes" if number even otherwise answer "no".'
-    question = random.randrange(0, 100)
+    question = f'Question: {random.randrange(0, 100)}'
     correct_answer = 'yes' if (question % 2 == 0) else 'no'
 
     return (question, correct_answer, rule)

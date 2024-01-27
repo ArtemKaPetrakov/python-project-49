89a7b9d66b636d0f37731ea3a5f6eec6236809a8
Artem Petrakov <artem.petrakov@climb-lab.ru>
2024-01-27 10:08:04 +0300

    change while to for/else, add game rounds in const

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,5 +1,7 @@
 import prompt
 
+GAME_ROUNDS = 3
+
 
 def game_run(game):
     print('Welcome to the Brain Games!')
@@ -8,19 +10,33 @@ def game_run(game):
     # question, correct_answer, rule = game.run_game()
     print(game.RULE)
 
-    count = 0
+    # count = 0
 
-    while count < 3:
+    # while count < 3:
+    #     question, correct_answer = game.run_game()
+    #     print(question)
+    #     user_answer = prompt.string('Your answer: ').strip()
+    #     if (user_answer != correct_answer):
+    #         print(f"'{user_answer}' is wrong answer ;(. "
+    #               f"Correct answer was '{correct_answer}'")
+    #         print(f"Let's try again, {name}!")
+    #         return
+    #         # Перенос строки в f шаблоне
+    #     elif (user_answer == correct_answer):
+    #         count += 1
+    #         print('Correct!')
+    # print(f"Congratulations, {name}!")
+
+    for _ in range(GAME_ROUNDS):
         question, correct_answer = game.run_game()
         print(question)
         user_answer = prompt.string('Your answer: ').strip()
-        if (user_answer != correct_answer):
+        if (user_answer == correct_answer):
+            print('Correct!')
+        else:
             print(f"'{user_answer}' is wrong answer ;(. "
                   f"Correct answer was '{correct_answer}'")
             print(f"Let's try again, {name}!")
             return
             # Перенос строки в f шаблоне
-        elif (user_answer == correct_answer):
-            count += 1
-            print('Correct!')
     print(f"Congratulations, {name}!")

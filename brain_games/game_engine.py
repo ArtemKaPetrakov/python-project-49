e92d7d5c1e77312b4a3773398649351b57a91781
Artem Petrakov <artem.petrakov@climb-lab.ru>
2024-01-27 10:10:54 +0300

    change while to for/else, add game rounds in const

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -7,25 +7,7 @@ def game_run(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ').strip()
     print(f'Hello, {name}!')
-    # question, correct_answer, rule = game.run_game()
     print(game.RULE)
-
-    # count = 0
-
-    # while count < 3:
-    #     question, correct_answer = game.run_game()
-    #     print(question)
-    #     user_answer = prompt.string('Your answer: ').strip()
-    #     if (user_answer != correct_answer):
-    #         print(f"'{user_answer}' is wrong answer ;(. "
-    #               f"Correct answer was '{correct_answer}'")
-    #         print(f"Let's try again, {name}!")
-    #         return
-    #         # Перенос строки в f шаблоне
-    #     elif (user_answer == correct_answer):
-    #         count += 1
-    #         print('Correct!')
-    # print(f"Congratulations, {name}!")
 
     for _ in range(GAME_ROUNDS):
         question, correct_answer = game.run_game()

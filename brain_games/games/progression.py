980f62d57ec14e93ecdac0cad0acc8e1b700b8d7
Artem Petrakov <artem.petrakov@climb-lab.ru>
2024-01-28 12:49:12 +0300

    put progression build in individual function

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,22 +3,44 @@ import random
 RULE = 'What number is missing in the progression?'
 
 
+def generate_progression():
+    start = int(random.randrange(1, 100))
+    step = int(random.randrange(1, 10))
+    stop = start + (step * 10)
+    # Условие, чтобы поместились все значения с нужным рандомным шагом
+    result = []
+
+    for number in range(start, stop, step):
+        result.append(str(number))
+    return result
+
+
 def run_game():
     # rule = 'What number is missing in the progression?'
-    progresion_length = 10
-    random_element = int(random.randrange(0, 9))
-    progresion_increment = int(random.randrange(1, 10))
-    star_number = int(random.randrange(1, 50))
-    result = []
-    count = 0
+    # progresion_length = 11
+    # random_element = int(random.randrange(0, 1))
+    # progresion_increment = int(random.randrange(1, 10))
 
-    while count < progresion_length:
-        result.append(str(star_number))
-        star_number += progresion_increment
-        count += 1
-    correct_answer = str(result[random_element])
-    result[random_element] = '..'
-    result = ' '.join(result)
+    # star_number = int(random.randrange(1, 50))
+
+    # for i in range(random_element, progresion_length, progresion_increment):
+    #     result.append(i)
+    # result = []
+    # count = 0
+
+    # while count < progresion_length:
+    #     result.append(str(star_number))
+    #     star_number += progresion_increment
+    #     count += 1
+
+    progresion = generate_progression()
+
+    random_element = random.randrange(0, 9)
+    # Выбираем рандомный элемент по индексу
+    correct_answer = progresion[random_element]
+
+    progresion[random_element] = '..'
+    result = ' '.join(progresion)
     question = f'Question: {result}'
 
     return (question, correct_answer)

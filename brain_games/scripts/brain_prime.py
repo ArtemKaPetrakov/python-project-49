1216ff7767f98f119b7ddc36d65132ad6d6c9fe8
Artem Petrakov <artem.petrakov@climb-lab.ru>
2024-02-03 19:05:28 +0300

    change name to run

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,9 +1,9 @@
-from brain_games.game_engine import game_run
+from brain_games.game_engine import run
 from brain_games.games import prime
 
 
 def main():
-    game_run(prime)
+    run(prime)
 
 
 if __name__ == '__main__':
